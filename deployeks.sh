c302871413426bf6e24452c094a9a9f62981e7d7
stevehwinwood <mail@s-winwood.com>
2019-06-06 20:56:48 +0100

    Initial commit

---
+++ deployeks.sh
@@ -0,0 +1,9 @@
+eksctl create cluster \
+--name steve-eks3 \
+--version 1.12 \
+--nodegroup-name standard-workers \
+--node-type t3.medium \
+--nodes 3 \
+--nodes-min 1 \
+--nodes-max 4 \
+--node-ami auto
